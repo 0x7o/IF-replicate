6f82bd675825b6f1f0c0941eb2470c383e399dd9
Danil <danil.kononyuk@ya.ru>
2023-05-01 21:29:02 +0800

    txt2img new version

--- inpainting/predict.py
+++ inpainting/predict.py
@@ -1,15 +1,11 @@
-from diffusers import (
-    IFInpaintingPipeline,
-    IFInpaintingSuperResolutionPipeline,
-    DiffusionPipeline,
-)
+from deepfloyd_if.modules import IFStageI, IFStageII, StableStageIII
+from deepfloyd_if.modules.t5 import T5Embedder
+from deepfloyd_if.pipelines import inpainting
 from cog import BasePredictor, Path, Input
-from diffusers.utils import pt_to_pil
 from PIL import Image
 from typing import List
 import huggingface_hub
 import random
-import torch
 import json
 
 
@@ -18,28 +14,12 @@ class Predictor(BasePredictor):
         with open("secret.json") as f:
             huggingface_token = json.load(f)["HUGGINGFACE_TOKEN"]
         huggingface_hub.login(huggingface_token)
-        self.stage_1 = IFInpaintingPipeline.from_pretrained(
-            "DeepFloyd/IF-I-XL-v1.0",
-            variant="fp16",
-            torch_dtype=torch.float16,
-            watermarker=None,
-        ).to("cuda")
-        self.stage_2 = IFInpaintingSuperResolutionPipeline.from_pretrained(
-            "DeepFloyd/IF-II-L-v1.0",
-            text_encoder=None,
-            variant="fp16",
-            torch_dtype=torch.float16,
-        ).to("cuda")
-        safety_modules = {
-            "feature_extractor": self.stage_1.feature_extractor,
-            "safety_checker": self.stage_1.safety_checker,
-            "watermarker": self.stage_1.watermarker,
-        }
-        self.stage_3 = DiffusionPipeline.from_pretrained(
-            "stabilityai/stable-diffusion-x4-upscaler",
-            **safety_modules,
-            torch_dtype=torch.float16,
-        ).to("cuda")
+
+        device = "cuda:0"
+        self.if_I = IFStageI("IF-I-XL-v1.0", device=device)
+        self.if_II = IFStageII("IF-II-L-v1.0", device=device)
+        self.if_III = StableStageIII("stable-diffusion-x4-upscaler", device=device)
+        self.t5 = T5Embedder(device=device)
 
     def clamp(self, value, min_value, max_value) -> int:
         return max(min(value, max_value), min_value)
@@ -63,12 +43,6 @@ class Predictor(BasePredictor):
         ),
         prompt: str = Input(description="Input prompt", default="blue sunglasses"),
         negative_prompt: str = "",
-        num_inference_steps: int = Input(
-            description="Number of inference steps",
-            default=50,
-            ge=1,
-            le=100,
-        ),
         num_outputs: int = Input(
             description="Number of images to output.",
             ge=1,
@@ -84,55 +58,39 @@ class Predictor(BasePredictor):
         guidance_scale: float = Input(
             description="Guidance scale", default=7.0, ge=0.0, le=10.0
         ),
-        stage3_upscale: bool = Input(
-            description="Use 1024x1024 upscaler", default=False
-        ),
     ) -> List[Path]:
         paths = []
         seed = random.randint(0, 2**32 - 1) if seed == 0 else seed
-        generator = torch.Generator(device="cuda").manual_seed(seed)
-        prompt_embeds, negative_embeds = self.stage_1.encode_prompt(
-            prompt=prompt, negative_prompt=negative_prompt, device="cuda"
-        )
         original_image = self.resize_image(Image.open(original_image).convert("RGB"))
         mask_image = self.resize_image(Image.open(mask_image).convert("RGB"))
-        for n in range(num_outputs):
-            image = self.stage_1(
-                image=original_image,
-                mask_image=mask_image,
-                prompt_embeds=prompt_embeds,
-                negative_prompt_embeds=negative_embeds,
-                generator=generator,
-                output_type="pt",
-                num_inference_steps=num_inference_steps,
-                guidance_scale=guidance_scale,
-            ).images
-            image = self.stage_2(
-                image=image,
-                mask_image=mask_image,
-                original_image=original_image,
-                prompt_embeds=prompt_embeds,
-                negative_prompt_embeds=negative_embeds,
-                guidance_scale=4.0,
-                generator=generator,
-                output_type="pt",
-                num_inference_steps=50,
-                noise_level=100,
-            ).images
-            if stage3_upscale:
-                image = self.stage_3(
-                    image=image,
-                    prompt=prompt,
-                    negative_prompt=negative_prompt,
-                    num_images_per_prompt=1,
-                    guidance_scale=9.0,
-                    num_inference_steps=75,
-                    generator=generator,
-                    noise_level=100,
-                ).images
-                image = image[0]
-            else:
-                image = pt_to_pil(image)[0]
+        result = inpainting(
+            t5=self.t5,
+            if_I=self.if_I,
+            if_II=self.if_II,
+            if_III=self.if_III,
+            disable_watermark=True,
+            support_pil_img=original_image,
+            negative_prompt=negative_prompt,
+            inpainting_mask=mask_image,
+            prompt=[prompt] * num_outputs,
+            seed=seed,
+            if_I_kwargs={
+                "guidance_scale": guidance_scale,
+                "sample_timestep_respacing": "10,10,10,10,10,0,0,0,0,0",
+                "support_noise_less_qsample_steps": 0,
+            },
+            if_II_kwargs={
+                "guidance_scale": 4.0,
+                "aug_level": 0.0,
+                "sample_timestep_respacing": "100",
+            },
+            if_III_kwargs={
+                "guidance_scale": 9.0,
+                "noise_level": 20,
+                "sample_timestep_respacing": "75",
+            },
+        )
+        for n, image in enumerate(result["III"]):
             image.save(f"/tmp/out-{n}.png")
             paths.append(Path(f"/tmp/out-{n}.png"))
         return paths
--- txt2img/predict.py
+++ txt2img/predict.py
@@ -1,10 +1,10 @@
+from deepfloyd_if.modules import IFStageI, IFStageII, StableStageIII
+from deepfloyd_if.modules.t5 import T5Embedder
 from cog import BasePredictor, Path, Input
-from diffusers import DiffusionPipeline
-from diffusers.utils import pt_to_pil
+from deepfloyd_if.pipelines import dream
 from typing import List
 import huggingface_hub
 import random
-import torch
 import json
 
 
@@ -13,39 +13,18 @@ class Predictor(BasePredictor):
         with open("secret.json") as f:
             huggingface_token = json.load(f)["HUGGINGFACE_TOKEN"]
         huggingface_hub.login(huggingface_token)
-        self.stage_1 = DiffusionPipeline.from_pretrained(
-            "DeepFloyd/IF-I-XL-v1.0",
-            variant="fp16",
-            torch_dtype=torch.float16,
-            watermarker=None,
-        ).to("cuda")
-        self.stage_2 = DiffusionPipeline.from_pretrained(
-            "DeepFloyd/IF-II-L-v1.0",
-            text_encoder=None,
-            variant="fp16",
-            torch_dtype=torch.float16,
-        ).to("cuda")
-        safety_modules = {
-            "feature_extractor": self.stage_1.feature_extractor,
-            "safety_checker": self.stage_1.safety_checker,
-            "watermarker": self.stage_1.watermarker,
-        }
-        self.stage_3 = DiffusionPipeline.from_pretrained(
-            "stabilityai/stable-diffusion-x4-upscaler",
-            **safety_modules,
-            torch_dtype=torch.float16,
-        ).to("cuda")
+
+        device = "cuda:0"
+        self.if_I = IFStageI("IF-I-XL-v1.0", device=device)
+        self.if_II = IFStageII("IF-II-L-v1.0", device=device)
+        self.if_III = StableStageIII("stable-diffusion-x4-upscaler", device=device)
+        self.t5 = T5Embedder(device=device)
 
     def predict(
         self,
         prompt: str = Input(description="Input prompt", default="A painting of a cat"),
         negative_prompt: str = "",
-        num_inference_steps: int = Input(
-            description="Number of inference steps",
-            default=50,
-            ge=1,
-            le=100,
-        ),
+        style_prompt: str = "",
         num_outputs: int = Input(
             description="Number of images to output.",
             ge=1,
@@ -58,8 +37,14 @@ class Predictor(BasePredictor):
             ge=0,
             le=2**32 - 1,
         ),
+        aug_level: float = Input(
+            description="Adds additional augmentation to generate more realistic images", default=0.25, ge=0.0, le=1.0
+        ),
         guidance_scale: float = Input(
             description="Guidance scale", default=7.0, ge=0.0, le=10.0
+        ),
+        aspect_ratio: str = Input(
+            description="Aspect ratio of the output image", default="1:1"
         ),
         stage3_upscale: bool = Input(
             description="Use 1024x1024 upscaler", default=False
@@ -67,43 +52,33 @@ class Predictor(BasePredictor):
     ) -> List[Path]:
         paths = []
         seed = random.randint(0, 2**32 - 1) if seed == 0 else seed
-        generator = torch.Generator(device="cuda").manual_seed(seed)
-        prompt_embeds, negative_embeds = self.stage_1.encode_prompt(
-            prompt=prompt, negative_prompt=negative_prompt, device="cuda"
+        result = dream(
+            t5=self.t5,
+            if_I=self.if_I,
+            if_II=self.if_II,
+            if_III=self.if_III,
+            disable_watermark=True,
+            negative_prompt=negative_prompt,
+            style_prompt=style_prompt,
+            aug_level=aug_level,
+            aspect_ratio=aspect_ratio,
+            prompt=[prompt] * num_outputs,
+            seed=seed,
+            if_I_kwargs={
+                "guidance_scale": guidance_scale,
+                "sample_timestep_respacing": "smart100",
+            },
+            if_II_kwargs={
+                "guidance_scale": 4.0,
+                "sample_timestep_respacing": "smart50",
+            },
+            if_III_kwargs={
+                "guidance_scale": 9.0,
+                "noise_level": 20,
+                "sample_timestep_respacing": "75",
+            },
         )
-        for n in range(num_outputs):
-            image = self.stage_1(
-                prompt_embeds=prompt_embeds,
-                negative_prompt_embeds=negative_embeds,
-                generator=generator,
-                output_type="pt",
-                num_inference_steps=num_inference_steps,
-                guidance_scale=guidance_scale,
-            ).images
-            image = self.stage_2(
-                image=image,
-                prompt_embeds=prompt_embeds,
-                negative_prompt_embeds=negative_embeds,
-                guidance_scale=4.0,
-                generator=generator,
-                output_type="pt",
-                num_inference_steps=50,
-                noise_level=250,
-            ).images
-            if stage3_upscale:
-                image = self.stage_3(
-                    image=image,
-                    prompt=prompt,
-                    negative_prompt=negative_prompt,
-                    num_images_per_prompt=1,
-                    guidance_scale=9.0,
-                    num_inference_steps=75,
-                    generator=generator,
-                    noise_level=100,
-                ).images
-                image = image[0]
-            else:
-                image = pt_to_pil(image)[0]
+        for n, image in enumerate(result["III"]):
             image.save(f"/tmp/out-{n}.png")
             paths.append(Path(f"/tmp/out-{n}.png"))
         return paths
